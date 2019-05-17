eeba4bdcb0e6a6f13e3123b9782e20aa2a353b72
xutaogh <39640123+xutaogh@users.noreply.github.com>
2019-05-17 11:52:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,23 +40,23 @@ curl  https://get.acme.sh | sh
 # 克隆V2ray.fun项目
 cd /usr/local/
 rm -R v2ray.fun
-git clone https://github.com/tracyone/v2ray.fun
+git clone https://github.com/xutaogh/v2
 
 # 安装V2ray主程序
 bash <(curl -L -s https://install.direct/go.sh)
 
 # 配置V2ray初始环境
-ln -sf /usr/local/v2ray.fun/v2ray /usr/local/bin
+ln -sf /usr/local/v2/v2ray /usr/local/bin
 chmod +x /usr/bin/v2ray
 chmod +x /usr/local/bin/v2ray
 rm -rf /etc/v2ray/config.json
-cp /usr/local/v2ray.fun/json_template/server.json /etc/v2ray/config.json
+cp /usr/local/v2/json_template/server.json /etc/v2ray/config.json
 let PORT=$RANDOM+10000
 UUID=$(cat /proc/sys/kernel/random/uuid)
 sed -i "s/cc4f8d5b-967b-4557-a4b6-bde92965bc27/${UUID}/g" /etc/v2ray/config.json
 sed -i "s/12345/${PORT}/g" "/etc/v2ray/config.json"
-python /usr/local/v2ray.fun/genclient.py
-python /usr/local/v2ray.fun/openport.py
+python /usr/local/v2/genclient.py
+python /usr/local/v2/openport.py
 service v2ray restart
 
 # auto open port after start
@@ -64,7 +64,7 @@ service v2ray restart
 cat /etc/rc.local | grep openport.py
 if [[ $? -ne 0 ]]; then
 cat>>/etc/rc.local<<EOF
-python /usr/local/v2ray.fun/openport.py
+python /usr/local/v2/openport.py
 EOF
 chmod a+x /etc/rc.local
 fi
@@ -72,5 +72,5 @@ fi
 
 clear
 
-echo "V2ray.fun 安装成功！"
+echo "V2 安装成功！"
 echo "输入 v2ray 回车即可使用"
